d22aaedf00a5e7fca217bd351ca19bd0c7d50650
Matthew <12hughesm@gmail.com>
2024-01-04 13:26:11 +0000

    Deployed and removed test data

--- src/data/taskData.ts
+++
@@ -1,9 +0,0 @@
-import { Task } from "../types/task";
-
-export const taskData: Task[] = [
-    { task: 'Complete coding assignment', completed: false },
-    { task: 'Read a programming book', completed: false },
-    { task: 'Exercise for 30 minutes', completed: false },
-    { task: 'Write documentation', completed: false },
-    { task: 'Attend team meeting', completed: false },
-];
