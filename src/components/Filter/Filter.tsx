586c4af23ad5bc77774d4302e3797dd462ceab1f
Matthew <12hughesm@gmail.com>
2024-01-04 16:29:19 +0000

    Can now sort by priority

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -2,19 +2,13 @@ import { ChangeEventHandler } from "react";
 
 type FilterProps = {
     onFilterPriorityChange: ChangeEventHandler<HTMLSelectElement>;
-    onFilterDateChange: ChangeEventHandler<HTMLSelectElement>;
 }
-export const Filter = ({onFilterDateChange, onFilterPriorityChange} : FilterProps) => {
+export const Filter = ({onFilterPriorityChange} : FilterProps) => {
   return (
     <div>
         <select name="priority" id="" onChange={onFilterPriorityChange}>
             <option value="asc">Asc</option>
             <option value="desc">Desc</option>
-        </select>
-
-        <select name="time" id="" onChange={onFilterDateChange}>
-            <option value="">Time Asc</option>
-            <option value="">Time Desc</option>
         </select>
     </div>
   )
