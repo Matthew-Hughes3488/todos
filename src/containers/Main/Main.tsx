43478ab01d6efd0b125672aa0b144cbc715a7cea
Matthew <12hughesm@gmail.com>
2024-01-04 09:57:50 +0000

    Finished basic HTML for main

---
+++ src/containers/Main/Main.tsx
@@ -0,0 +1,15 @@
+import { Header } from "../../components/Header/Header"
+
+
+export const Main = () => {
+  return (
+    <main>
+        <Header/>
+        {
+            //inputBar
+            //TaskList
+            //Show message if task list is empty instead
+        }
+    </main>
+  )
+}
