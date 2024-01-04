5fd2f9abc4bfc7dfc7caf277a63a2b34233d8b94
Matthew <12hughesm@gmail.com>
2024-01-04 16:32:38 +0000

    Finished styles

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -8,8 +8,8 @@ export const Filter = ({onFilterPriorityChange} : FilterProps) => {
   return (
     <div>
         <select className="dropdown-menu" name="priority" id="" onChange={onFilterPriorityChange}>
-            <option value="asc">Asc</option>
-            <option value="desc">Desc</option>
+            <option value="asc">Priority Asc</option>
+            <option value="desc">Priority Desc</option>
         </select>
     </div>
   )
