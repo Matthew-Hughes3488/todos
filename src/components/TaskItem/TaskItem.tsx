2ff6bf23dbefbe127afabf81740c328e543ce65c
Matthew <12hughesm@gmail.com>
2024-01-04 11:43:20 +0000

    Added task id's. Used to delete specifc tasks when pressed

--- src/components/TaskItem/TaskItem.tsx
+++ src/components/TaskItem/TaskItem.tsx
@@ -3,14 +3,19 @@ import "./TaskItem.scss";
 
 type TaskItemProps = {
   task: Task;
+  onDelete: (taskId: number) => void
 };
 
-export const TaskItem = ({ task }: TaskItemProps) => {
+export const TaskItem = ({ task, onDelete }: TaskItemProps) => {
+  const handleBin = () =>{
+    onDelete(task.id);
+  }
+
   return (
-    <section className="task-card">
+    <section className="task-card" id={`task-card-${task.id}`}>
         <input className="task-card__checkbox" type="checkbox" />
         <p className="task-card__task">{task.task}</p>
-      <button className="task-card__bin">Bin</button>
+      <button className="task-card__bin" onClick={handleBin}>Bin</button>
     </section>
   );
 };
--- src/components/TaskList/TaskList.tsx
+++ src/components/TaskList/TaskList.tsx
@@ -2,14 +2,15 @@ import { Task } from "../../types/task"
 import { TaskItem } from "../TaskItem/TaskItem"
 
 type TaskListProps = {
-    tasks: Task[]
+    tasks: Task[],
+    onDelete: (taskId: number) => void
 }
 
-export const TaskList = ({tasks} : TaskListProps) => {
+export const TaskList = ({tasks, onDelete} : TaskListProps) => {
   return (
     <section className='task-list-container'>
         {tasks.map(task =>{
-            return <TaskItem task={task}/>
+            return <TaskItem task={task} onDelete={onDelete}/>
         })}
     </section>
   )
