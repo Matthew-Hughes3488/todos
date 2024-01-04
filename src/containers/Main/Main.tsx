ef35e2d57620fbb9908b63f1edcc354dc2ab5c4b
Matthew <12hughesm@gmail.com>
2024-01-04 13:29:02 +0000

    Fixed bug with adding empty tasks

--- src/containers/Main/Main.tsx
+++ src/containers/Main/Main.tsx
@@ -8,7 +8,7 @@ import "./Main.scss";
 export const Main = () => {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [taskInput, setTaskInput] = useState("");
-  const [numberOfTasksAdded, setNumberOfTasksAdded] = useState(0)
+  const [numberOfTasksAdded, setNumberOfTasksAdded] = useState(0);
 
   const handleTaskInput = (event: ChangeEvent<HTMLInputElement>) => {
     const value: string = event.currentTarget.value;
@@ -18,30 +18,36 @@ export const Main = () => {
   const handleNewTask = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
-    const newTask: Task = { id: numberOfTasksAdded, task: taskInput};
-    setNumberOfTasksAdded(numberOfTasksAdded + 1);
-    
-    setTasks((prevTasks) => [...prevTasks, newTask]);
-    setTaskInput("")
+    if (taskInput) {
+      const newTask: Task = { id: numberOfTasksAdded, task: taskInput };
+      setNumberOfTasksAdded(numberOfTasksAdded + 1);
+
+      setTasks((prevTasks) => [...prevTasks, newTask]);
+      setTaskInput("");
+    }
   };
 
-  const deleteTask = (taskId: number) =>{
-    const newTasks : Task[] = tasks.filter(task =>{
+  const deleteTask = (taskId: number) => {
+    const newTasks: Task[] = tasks.filter((task) => {
       return taskId != task.id;
-    })
+    });
     setTasks(newTasks);
-  }
+  };
 
-  const handleReset = () =>{
+  const handleReset = () => {
     setTasks([]);
-  }
+  };
 
   return (
     <main className="task-webpage">
-      <Header onClick={handleReset}/>
-      <InputBar onSubmit={handleNewTask} onChange={handleTaskInput} taskValue={taskInput}/>
+      <Header onClick={handleReset} />
+      <InputBar
+        onSubmit={handleNewTask}
+        onChange={handleTaskInput}
+        taskValue={taskInput}
+      />
       {tasks.length > 0 ? (
-        <TaskList tasks={tasks} onDelete={deleteTask}/>
+        <TaskList tasks={tasks} onDelete={deleteTask} />
       ) : (
         <p className="task-webpage__text">Add tasks here 👆</p>
       )}
